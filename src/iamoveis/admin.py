22cc1f0c3b5ef7e3232c061b2438d8a316b01e7f
hevdev21 <heverton.gomes2018@gmail.com>
2026-07-21 00:44:22 +0000

    feat:comando para fazer carga de imóveis via csv ou json.

--- src/iamoveis/admin.py
+++ src/iamoveis/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Imovel
 
 # Register your models here.
+
+@admin.register(Imovel)
+class ImovelAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'tipo_transacao', 
+        'bairro', 
+        'preco', 
+        'quartos', 
+        'origem_carga', 
+        'created_at'
+    )
+    list_filter = ('tipo_transacao', 'bairro', 'quartos', 'origem_carga')
+    search_fields = ('pk', 'bairro', 'endereco', 'descricao', 'origem_carga')
+    ordering = ('-created_at',)
