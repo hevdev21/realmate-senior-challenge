df30da8e1274329f0f2fca0c89f0ec68785ef4ec
hevdev21 <heverton.gomes2018@gmail.com>
2026-07-21 00:10:06 +0000

    feat:inicialização dos modelos base para implementação do projeto.

---
+++ src/iamoveis/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Imovel(models.Model):
+    TIPO_TRANSACAO_CHOICES = [
+        ('aluguel', 'Aluguel'),
+        ('venda', 'Venda'),
+    ]
+    tipo_transacao = models.CharField(max_length=10, choices=TIPO_TRANSACAO_CHOICES,  null=True, blank=True, db_index=True)
+    bairro = models.CharField(max_length=250, null=True, blank=True)
+    endereco = models.CharField(max_length=250, null=True, blank=True)
+    preco = models.DecimalField(max_digits=12,decimal_places=2, null=True, blank=True)
+    quartos = models.IntegerField(null=True, blank=True)
+    descricao = models.TextField(null=True, blank=True)
+
+    origem_carga = models.CharField(max_length=100, null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Conversa(models.Model):
+    STATUS_CHOICES = [
+        ('active', 'Ativa'),
+        ('closed', 'Fechada'),
+    ]
+
+    imoveis_recomendados = models.ManyToManyField(
+        Imovel, 
+        related_name='conversas',
+        blank=True
+    )
+    telefone_cliente = models.CharField(max_length=250, null=True, blank=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='active', db_index=True)
+    last_message_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Mensagem(models.Model):
+    ROLE_CHOICES = [
+        ('customer', 'Cliente'),
+        ('assistant', 'Assistente'),
+    ]
+
+    conversa = models.ForeignKey(
+        Conversa, 
+        on_delete=models.CASCADE, 
+        related_name='mensagens'
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    conteudo = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
